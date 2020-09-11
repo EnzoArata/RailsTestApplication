cfa72aef4d6dcb29e2d59b1506c384b44aaedf33
Enzo Arata <enzoarata50@nevada.unr.edu>
2020-09-11 09:13:37 -0700

    Added login/logout functionality by creating sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
-
+      session[:user_id] = user.id
       flash[:notice] = "Logged in succesfuly"
       redirect_to user
     else
@@ -17,6 +16,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles
   end
 
@@ -13,11 +13,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account succesfuly updated"
       redirect_to @user
@@ -28,6 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to Room8,#{@user.username} you have succesfuly signed up"
         redirect_to articles_path
       else
@@ -41,4 +40,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
