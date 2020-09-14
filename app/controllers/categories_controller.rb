dfca3103eeedad6e4c3273d263b35a8f73323aa5
Enzo Arata <enzoarata50@nevada.unr.edu>
2020-09-14 14:28:52 -0700

    Added ability to create category from browser

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,31 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category succesfully saved"
+      redirect_to @category
+    else
+      render "new"
+    end
   end
 
   def show
+
   end
 
   def index
-  end 
+
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
